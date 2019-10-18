4fd28c42308c381f3c686bbfb5576689a089e301
Yining Wang <yining.wang@utexas.edu>
2019-10-18 21:42:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4, 
       rent_in_dollars: {
-        one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: {}, four: {}
+        one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four: {}
       },
       names: {
         one: {}, two: {}, three: {}, four: {}
