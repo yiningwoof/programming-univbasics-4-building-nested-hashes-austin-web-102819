433ef5188954b396a06465cd7b32c5bc66fc4dba
Yining Wang <yining.wang@utexas.edu>
2019-10-18 21:30:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{cat: 'chaichai'}
+	{railroads: {}}
 end
 
 def monopoly_with_second_tier
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads => {}}
+  {railroads: {}}
 end
 
 def monopoly_with_third_tier
