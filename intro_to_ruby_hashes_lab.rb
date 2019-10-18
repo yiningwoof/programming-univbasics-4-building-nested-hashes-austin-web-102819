3461693b5e2cf86d4068faeef8245e391c84fe04
Yining Wang <yining.wang@utexas.edu>
2019-10-18 21:39:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,9 @@ def monopoly_with_third_tier
       rent_in_dollars: {
         one: {}, two: {}, three: {}, four: {}
       },
-      names: {}
+      names: {
+        one: {}, two: {}, three: {}, four: {}
+      }
     }
   }
 end
